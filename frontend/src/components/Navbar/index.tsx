11784798fc579869d84ae50c182ea9920de9e3f2
Beto Seideman <betoseideman@gmail.com>
2022-01-15 15:37:22 -0300

    trocar nome link

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -9,7 +9,7 @@ function Navbar() {
                     <a href="https://github.com/betoseideman">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsupeior</p>
+                            <p className="dsmovie-contact-link">/betoseideman</p>
                         </div>
                     </a>
                 </div>
